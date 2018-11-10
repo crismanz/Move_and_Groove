5df30b666c23b564520c3639a9cdbcba9e79a62b
crissmanz <cristina.manzoni@outlook.com>
2018-11-10 14:05:37 +0000

    add devise for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    activities_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+
 end
